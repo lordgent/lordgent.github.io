0458026087cb631a40d012c7b9c403c3479c41bc
lordgent <lorjenx08@gmail.com>
2021-02-01 22:26:19 +0700

    project selesai

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ const sidenav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sidenav);
 
 const slider = document.querySelectorAll('.slider');
-M.Slider.init(slider);
+M.Slider.init(slider, {
+    
+    indicators: false,
+    transition: 600,
+    interval: 3000
+
+
+});
